7f435529b2aa9592212f9f66306e19e9808f3396
WANGHYOMIN <twomin2ho@gmail.com>
2025-09-12 19:37:23 +0900

    Add files via upload

---
+++ chapter02/chapter02_02/chapter02_02_practice 5.py
@@ -0,0 +1,33 @@
+#chapter02_02_practice 5 (2025_09_12)
+"""문자열 인덱싱과 슬라이싱"""
+a="If you are infected with bacteria, you should take antibiotics"
+print(a[5])
+print(a[-2])
+print(a[-1])
+print(a[0])
+print(a[-0])
+
+b=a[3]+a[4]+a[5]
+print(b)
+
+print(a[3:6])
+
+print(a[7:])
+
+print(a[:8])
+
+print(a[:])
+
+print(a[3:-12])
+
+c="20250912Rainy"
+date=c[:8]
+weather=c[8:]
+print(date)
+print(weather)
+
+d="Pithon"
+print(d[0]+'y'+ d[2:])
+
+
+
